daff6925d22b12a0eb57a4bac160982c66200300
Kamontat Chantrachirathumrong <kamontat.chantrachirathumrong@agoda.com>
2020-06-25 12:11:05 +0700

    feat(utils): add -all in root-clean command

--- internals/commandline/src/scripts/root/clean.ts
+++ internals/commandline/src/scripts/root/clean.ts
@@ -1,8 +1,12 @@
 import { lerna } from "../lerna/core";
 
-const cli = lerna(({ helper }) => {
+const cli = lerna(({ data, helper }) => {
+  const isAll: boolean = data.all;
+
   const args: string[] = ["run", "clean"];
   if (helper.env.isCI()) args.push("--concurrency", "1");
+  if (isAll) args.push("--", "--all");
+
   return { arguments: args };
 });
 
