76cd15a0cbe873ecd40e08177a7ecc884e589a0b
Kamontat Chantrachirathumrong <kamontat.chantrachirathumrong@agoda.com>
2020-06-25 13:35:03 +0700

    fix(utils): remove --on from root-run, use scope instead

--- internals/commandline/src/scripts/root/run.ts
+++ internals/commandline/src/scripts/root/run.ts
@@ -3,10 +3,7 @@ import { lerna } from "../lerna/core";
 const cli = lerna(async ({ data }) => {
   if (data.build) require("./build");
 
-  const on = data.on;
-  if (!on) throw new Error("must have --on=<package_name>");
-
-  const args: string[] = ["exec", "yarn", "start", "--scope", on];
+  const args: string[] = ["exec", "yarn", "start"];
   return { arguments: args };
 });
 
