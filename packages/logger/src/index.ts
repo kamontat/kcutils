953456334f25e465a35f24e6f80f792aee1dc3e7
Kamontat Chantrachirathumrong <kamontat.chantrachirathumrong@agoda.com>
2020-06-29 18:04:59 +0700

    perf(model): export more interface and utils

--- packages/logger/src/index.ts
+++ packages/logger/src/index.ts
@@ -1,2 +1,14 @@
 export { Logger } from "./models/logger/Logger";
 export { WithLogger } from "./services/WithLogger";
+
+export { settingBuilder } from "./constants/settings";
+export { toLevel } from "./constants/levels";
+
+export type { LoggerLevel } from "./models/logger/LoggerLevel";
+export type { StrictSettingObject } from "./models/logger/LoggerSetting";
+export type { DefaultKeyTypes } from "./constants/types";
+
+export type { InputMessage } from "./models/input/InputMessage";
+export type { InputOption } from "./models/input/InputOptions";
+
+export type { OutputMessage } from "./models/output/OutputMessage";
