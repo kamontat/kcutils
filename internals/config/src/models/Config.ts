fa34b7370a07df429fab11506412af595925bd03
Kamontat Chantrachirathumrong <developer@kamontat.net>
2020-07-11 16:49:25 +0700

    fix(model): helper current of commandline helper not resolve to configuration

--- internals/config/src/models/Config.ts
+++ internals/config/src/models/Config.ts
@@ -7,6 +7,7 @@ export class Config<C, R> extends Setting<Required<C>, R> {
   constructor(builder: ConfigBuilder<C, R>, input?: Partial<C>, dirpath: string = process.cwd()) {
     super({
       dirname: dirpath,
+      current: ["@kcinternal", "configuration"],
       input: byDefault({} as C, builder.default, input ?? {}) as Required<C>,
       transform: builder.transformer,
     });
