237c3e166681657a84df0a2dd1262dcd596cec4b
Kamontat Chantrachirathumrong <kamontat.chantrachirathumrong@agoda.com>
2020-07-16 08:47:23 +0700

    perf(service): add support custom command on root-run (default is start)

--- internals/commandline/src/scripts/root/run.ts
+++ internals/commandline/src/scripts/root/run.ts
@@ -2,8 +2,8 @@ import { lerna } from "../lerna/core";
 
 const cli = lerna(async ({ data }) => {
   if (data.build) require("./build");
-
-  const args: string[] = ["exec", "yarn", "start"];
+  const commands = data._;
+  const args: string[] = ["exec", "yarn", ...(commands.length > 0 ? commands : ["start"])];
   return { arguments: args };
 });
 
