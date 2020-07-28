646b3183e5d933f3c416cc72453e9bad2598ad86
Kamontat Chantrachirathumrong <kamontat.chantrachirathumrong@agoda.com>
2020-07-29 00:55:15 +0700

    chore(test): add more test in color object

--- packages/color/test/index.test.ts
+++ packages/color/test/index.test.ts
@@ -7,11 +7,11 @@ describe("Color object", () => {
     }).toThrow();
   });
 
-  test.each([[{ a: 1, type: "number", r: 24, g: 12, b: 100 } as RGB, "rgb(24,12,100)"]])(
-    "create '%s' color should equals to '%s'",
-    (input: RGB, output: string) => {
-      const c = new Color(input);
-      expect(c.toString()).toEqual(output);
-    }
-  );
+  test.each([
+    [{ type: "number", r: 24, g: 12, b: 100 } as RGB, "rgb(24,12,100)"],
+    [{ a: 1, type: "number", r: 24, g: 12, b: 100 } as RGB, "rgb(24,12,100)"],
+  ])("create '%s' color should equals to '%s'", (input: RGB, output: string) => {
+    const c = new Color(input);
+    expect(c.toString()).toEqual(output);
+  });
 });
