a64a9a48bd533eb801a26a40e3e90a9ac33f9cea
Kamontat Chantrachirathumrong <kamontat.chantrachirathumrong@agoda.com>
2020-08-01 17:28:51 +0700

    perf(model): export optional typescript type as type

--- packages/helper/src/index.ts
+++ packages/helper/src/index.ts
@@ -6,6 +6,8 @@ import * as json from "./types/json";
 import * as string from "./types/string";
 import * as path from "./types/path";
 
+import * as type from "./models/Optional";
+
 import stream from "./types/stream";
 
-export { generic, env, array, json, string, path, stream };
+export { generic, env, array, json, string, path, stream, type };
