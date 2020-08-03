46e2d3812ff3718937fcf2f18ed8228bacef19d9
Kamontat Chantrachirathumrong <kamontat.chantrachirathumrong@agoda.com>
2020-08-03 23:19:38 +0700

    fix(utils): getOrElse will return else when input is empty string

--- internals/commandline/src/models/common/helpers/GeneralHelper.ts
+++ internals/commandline/src/models/common/helpers/GeneralHelper.ts
@@ -1,6 +1,7 @@
 export class GeneralHelper {
   getOrElse<T>(data: T | undefined | null, def: T): T {
     if (data === undefined || data === null) return def;
+    else if (typeof data === "string" && data === "") return def;
     else return data;
   }
 }
