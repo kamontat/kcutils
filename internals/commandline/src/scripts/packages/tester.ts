0709ac3ba747909091ce68b32507a9aebd37cbf7
Kamontat Chantrachirathumrong <kamontat.chantrachirathumrong@agoda.com>
2020-08-03 23:14:14 +0700

    perf(core): add --stryker to itester but run only when @kcutils/testkit exist

--- internals/commandline/src/scripts/packages/tester.ts
+++ internals/commandline/src/scripts/packages/tester.ts
@@ -1,9 +1,23 @@
 import { AsyncRunner, Commandline, Option } from "../..";
 
 const option = new Option({ dirname: process.cwd(), input: process.argv.slice(2), transform: Option.transform });
-const transformer = new AsyncRunner(option, async ({ helper }) => {
+const transformer = new AsyncRunner(option, async ({ helper, data }) => {
+  const args = helper.argument.parse(data.arguments, { default: { stryker: false } });
+
   const config = await helper.on("parent").pathEnsure("jest.config.js");
   const jest = helper.path.nodeCommand("jest");
+
+  // support stryker in testkit devDependencies
+  if (args.stryker && helper.on("parent").searchPackageJsonSync("devDependencies", "@kcutils/testkit")) {
+    helper.log.debug("stryker", "enable stryker test");
+    const testkitPath = helper.on("parent").pathEnsureSync("node_modules", "@kcutils", "testkit");
+    const strykerCli = (testkitPath ? helper.path.add(testkitPath) : helper.path).nodeCommand("stryker");
+    if (strykerCli !== undefined) {
+      return [strykerCli, "run"];
+    } else {
+      helper.log.debug("stryker", "cannot run because stryker path not found");
+    }
+  }
 
   if (jest === undefined) {
     return ["echo", "[skip] jest command not found"];
