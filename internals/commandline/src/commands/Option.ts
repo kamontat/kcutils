f054656b3af029effb15c32ab616843b57851a94
Kamontat Chantrachirathumrong <kamontat.chantrachirathumrong@agoda.com>
2020-08-05 18:09:50 +0700

    fix(model): changes static transformer on Option to support dryrun parameters

--- internals/commandline/src/commands/Option.ts
+++ internals/commandline/src/commands/Option.ts
@@ -5,7 +5,11 @@ import { CommandlineOption } from "./Commandline";
 export class Option<O = string[]> extends DataBuilder<string[], O> {
   static transform: TransformFn<InputWrapper<string[], DataBuilderHelperString>, Promise<CommandlineOption>> = ({
     data,
+    helper,
   }) => {
-    return Promise.resolve({ dryrun: false, arguments: data });
+    const args = helper.argument.parse(data, { alias: { dryrun: ["dry", "dryrun", "dry-run", "d"] } });
+    const dryrun = args.dry || args.dryrun || args["dry-run"] || args._.includes("dry");
+
+    return Promise.resolve({ dryrun: dryrun, arguments: args, raw: data });
   };
 }
