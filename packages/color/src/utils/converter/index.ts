904da13e52767c562b6c680e1f64849f2dea3c02
Kamontat Chantrachirathumrong <kamontat.chantrachirathumrong@agoda.com>
2020-08-08 15:06:13 +0700

    chore: export more method from converter function

--- packages/color/src/utils/converter/index.ts
+++ packages/color/src/utils/converter/index.ts
@@ -2,5 +2,5 @@ export { toDecimal, toNumber, toPercentage, toType } from "./type";
 
 export { hslToRgb, hslToHsl } from "./hsl";
 export { hsvToRgb, hsvToHsv } from "./hsv";
-export { rgbToRgb, rgbToHsl, rgbToHsv, rgbToHex } from "./rgb";
+export { defaultRGB, rgbToRgb, rgbToHsl, rgbToHsv, rgbToHex, rgbToNamed, enforceRGB, roundedRGB } from "./rgb";
 export type { RGBHexOptions } from "./rgb";
