e2441aa7c6702da452b32c7fa0fe11a2a3095c35
Kamontat Chantrachirathumrong <kamontat.chantrachirathumrong@agoda.com>
2020-08-08 23:28:42 +0700

    refac: remove unused method name isOnePointZero()

--- packages/color/src/utils/checker.ts
+++ packages/color/src/utils/checker.ts
@@ -1,17 +1,6 @@
 import { NumberType, Type } from "../typings/NumberType";
 import { colorMatchers } from "./constants";
 
-/**
- * Need to handle 1.0 as 100%, since once it is a number, there is no difference between it and 1
- * <http://stackoverflow.com/questions/7422072/javascript-how-to-detect-number-as-a-decimal-including-1-0>
- *
- * @param n check input is 1.0
- */
-export const isOnePointZero = (n: string | number): boolean => {
-  if (typeof n === "number") return n === 1;
-  return n.indexOf(".") !== -1 && parseFloat(n) === 1;
-};
-
 export const isValidCSSUnit = (color: string): boolean => {
   return colorMatchers.cssUnit.test(color);
 };
