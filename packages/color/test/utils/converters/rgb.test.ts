0caa94e40b1b2358492ce659b55f974e1fabded9
Kamontat Chantrachirathumrong <kamontat.chantrachirathumrong@agoda.com>
2020-08-09 19:03:53 +0700

    chore(test): add more rgb converter testcase

--- packages/color/test/utils/converters/rgb.test.ts
+++ packages/color/test/utils/converters/rgb.test.ts
@@ -1,6 +1,5 @@
-import { RGB, HEX, Named, NumberTypeString, HexTypeString, HSL, converter } from "../../../src";
-
-const { enforceRGB, roundedRGB, rgbToRgb, rgbToHex, rgbToNamed, rgbToHsl } = converter;
+import { RGB, HEX, Named, NumberTypeString, HexTypeString, HSL, converter, HSV } from "../../../src";
+const { enforceRGB, roundedRGB, rgbToRgb, rgbToHex, rgbToNamed, rgbToHsl, rgbToHsv } = converter;
 
 type EachType<O, T> = [RGB, O, T];
 type EachTypes<O, T> = EachType<O, T>[];
@@ -81,6 +80,16 @@ describe("RGB to RGB", () => {
         { r: 12, g: 99, b: 200, type: type.number } as RGB,
         type.decimal,
         { r: 0.05, g: 0.39, b: 0.78, a: 1, type: type.decimal } as RGB,
+      ],
+      [
+        { r: 0.423, g: 0.445, b: 0.98499, type: type.decimal } as RGB,
+        type.number,
+        { r: 107.87, g: 113.48, b: 251.17, a: 1, type: type.number } as RGB,
+      ],
+      [
+        { r: 51.33, g: 51.35, b: 51.457, type: type.percent } as RGB,
+        type.number,
+        { r: 130.89, g: 130.94, b: 131.22, a: 1, type: type.number } as RGB,
       ]
     )
   )("convert RGB(%p, %s) to RGB(%p)", (rgb, type, result) => {
@@ -135,10 +144,31 @@ describe("RGB to HSL color", () => {
   };
 
   test.each([
-    [{ r: 123, g: 234, b: 12, type: "number" } as RGB, definedHSL({ h: 0.25, s: 0.9, l: 0.49 })], // no alpha
     [{ type: "number" } as RGB, definedHSL()],
+    [{ r: 123, g: 234, b: 12, type: "number" } as RGB, definedHSL({ h: 0.25, s: 0.9, l: 0.49 })], // no alpha
+    [{ r: 36, g: 0, b: 194, type: "number" } as RGB, definedHSL({ h: 0.7, s: 1, l: 0.38 })],
+    [{ r: 222, g: 51, b: 1, a: 0.555, type: "number" } as RGB, definedHSL({ h: 0.04, s: 1, l: 0.44, a: 0.56 })],
+    [{ r: 222, g: 99, b: 111, a: 0.003, type: "number" } as RGB, definedHSL({ h: 0.98, s: 0.65, l: 0.63, a: 0 })],
   ])("convert RGB(%p) to HSL(%p)", (rgb, hsl) => {
     expect(rgbToHsl(rgb)).toEqual(hsl);
+  });
+});
+
+describe("RGB to HSV color", () => {
+  const definedHSV = (input?: Partial<HSV>): HSV => {
+    const defaultHSV: HSV = { a: 1, h: 0, v: 0, s: 0, type: "decimal" };
+    if (input) return Object.assign({}, defaultHSV, input);
+    else return Object.assign({}, defaultHSV);
+  };
+
+  test.each([
+    [{ type: "number" } as RGB, definedHSV()],
+    [{ r: 123, g: 234, b: 12, type: "number" } as RGB, definedHSV({ h: 0.25, s: 0.95, v: 0.92 })], // no alpha
+    [{ r: 36, g: 0, b: 194, type: "number" } as RGB, definedHSV({ h: 0.7, s: 1, v: 0.76 })],
+    [{ r: 222, g: 51, b: 1, a: 0.555, type: "number" } as RGB, definedHSV({ h: 0.04, s: 1, v: 0.87, a: 0.56 })],
+    [{ r: 222, g: 99, b: 111, a: 0.003, type: "number" } as RGB, definedHSV({ h: 0.98, s: 0.55, v: 0.87, a: 0 })],
+  ])("convert RGB(%p) to HSV(%p)", (rgb, hsv) => {
+    expect(rgbToHsv(rgb)).toEqual(hsv);
   });
 });
 
