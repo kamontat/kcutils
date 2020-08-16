b6707ce0c3a98a795feb2e6b76e1119086d65d10
Kamontat Chantrachirathumrong <kamontat.chantrachirathumrong@agoda.com>
2020-08-16 18:53:28 +0700

    feat(model): Logger option now support environment variable prefix with KCUTILS_

--- packages/logger/src/constants/options.ts
+++ packages/logger/src/constants/options.ts
@@ -1,16 +1,20 @@
 import { arrowRight } from "figures";
+import { Writable } from "stream";
+import { env } from "@kcutils/helper";
 
 import { StrictOption } from "../models/logger/LoggerOption";
-import { Writable } from "stream";
+import { toLevel } from "./levels";
+
+export const prefix = "KCUTILS_";
 
 export const options: StrictOption = {
-  debug: false,
+  debug: env.read(prefix + "DEBUG", "false") === "true",
   output: ["file", "console"],
-  json: false,
-  interactive: false,
-  disabled: false,
-  color: true,
-  level: "info",
+  json: env.read(prefix + "JSON", "false") === "true",
+  interactive: env.read(prefix + "INTERACTIVE", "false") === "true",
+  disabled: env.read(prefix + "DISABLED", "false") === "true",
+  color: env.read(prefix + "COLORS", "false") === "true",
+  level: toLevel(env.read(prefix + "LEVEL", "info")).name,
   datetime: "date",
   censor: () => "secure",
   separator: arrowRight,
