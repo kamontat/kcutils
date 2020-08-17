f6471f7d6df2fe2332197dd68710be87f00218da
Kamontat Chantrachirathumrong <kamontat.chantrachirathumrong@agoda.com>
2020-08-17 20:36:11 +0700

    fix(lint): compile error due to lint check

--- packages/helper/src/types/json.ts
+++ packages/helper/src/types/json.ts
@@ -91,25 +91,25 @@ export const forceObject = <T = unknown>(obj: Optional<T>, def = {}): T => {
  * @param key object query statment (format as a.b.c)
  * @param all if enable this, getObject will return full object instead of undefined
  */
-export const getObject = <T extends NestedJson, R extends any>(
+export const getObject = <R extends any, T extends NestedJson<R>>(
   obj: T,
   key: string,
   all: boolean = false
-): R | undefined => {
+): R | T | undefined => {
   const _obj: T = forceObject(obj);
-  if (!isString(key)) return all ? (_obj as R) : undefined;
+  if (!isString(key)) return all ? _obj : undefined;
 
   const keys = key.split(".");
   const result: any = keys.reduce((p, k) => {
     if (p[k]) return p[k];
     else return p;
-  }, _obj);
+  }, _obj as any);
 
   // if result is the same as input return 'undefined' or 'input' base on `all`
   if (result !== _obj && nonEmpty(result)) {
     return result;
   } else {
-    return all ? (_obj as R) : undefined;
+    return all ? _obj : undefined;
   }
 };
 
