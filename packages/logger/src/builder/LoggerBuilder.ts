947c5f5e55d698019232cc7bb0d9472a6e1a15f4
Kamontat Chantrachirathumrong <developer@kamontat.net>
2020-09-08 18:24:40 +0700

    feat(model): add LoggerBuilder to make logger easier to create

---
+++ packages/logger/src/builder/LoggerBuilder.ts
@@ -0,0 +1,32 @@
+import { Logger } from "../models/logger/Logger";
+import { LoggerOptionBuilder } from "./LoggerOptionBuilder";
+
+export class LoggerBuilder<T extends string> {
+  static initial<T extends string = "">(): LoggerBuilder<T> {
+    return new LoggerBuilder<T>();
+  }
+
+  private builder: LoggerOptionBuilder<string>;
+
+  private constructor() {
+    this.builder = LoggerOptionBuilder.initial();
+  }
+
+  withOption<R extends string>(builder: LoggerOptionBuilder<R>): LoggerBuilder<R> {
+    this.builder = builder;
+    return this as LoggerBuilder<R>;
+  }
+
+  updateOption<R extends string>(
+    builder: (b: LoggerOptionBuilder<T>) => LoggerOptionBuilder<T | R>
+  ): LoggerBuilder<T | R> {
+    const newBuilder = builder(this.builder);
+    this.builder = newBuilder;
+
+    return this as LoggerBuilder<T | R>;
+  }
+
+  get(): Logger<T> {
+    return Logger.create<T>();
+  }
+}
