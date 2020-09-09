0dbbbe8e9a6ccb83f4faaf64ff4aef63b18e1e2d
Kamontat Chantrachirathumrong <developer@kamontat.net>
2020-09-09 12:06:57 +0700

    refac: fix recursive call in logger level

--- packages/logger/src/constants/levels.ts
+++ packages/logger/src/constants/levels.ts
@@ -2,38 +2,12 @@ import { stream } from "@kcutils/helper";
 
 import { LoggerLevel, Levels } from "../models/logger/LoggerLevel";
 
-import { LoggerLevelBuilder } from "../builder/LoggerLevelBuilder";
-
-export const silent: LoggerLevel = LoggerLevelBuilder.new()
-  .withLevel(0)
-  .withName("silent")
-  .withStream(stream.null)
-  .get();
-export const error: LoggerLevel = LoggerLevelBuilder.new()
-  .withLevel(1)
-  .withName("error")
-  .withStream(process.stderr)
-  .get();
-export const warn: LoggerLevel = LoggerLevelBuilder.new()
-  .withLevel(2)
-  .withName("warn")
-  .withStream(process.stderr)
-  .get();
-export const info: LoggerLevel = LoggerLevelBuilder.new()
-  .withLevel(3)
-  .withName("info")
-  .withStream(process.stdout)
-  .get();
-export const debug: LoggerLevel = LoggerLevelBuilder.new()
-  .withLevel(4)
-  .withName("debug")
-  .withStream(process.stdout)
-  .get();
-export const silly: LoggerLevel = LoggerLevelBuilder.new()
-  .withLevel(5)
-  .withName("silly")
-  .withStream(process.stdout)
-  .get();
+export const silent: LoggerLevel = new LoggerLevel(0, "silent", stream.null);
+export const error: LoggerLevel = new LoggerLevel(1, "error", process.stderr);
+export const warn: LoggerLevel = new LoggerLevel(2, "warn", process.stderr);
+export const info: LoggerLevel = new LoggerLevel(3, "info", process.stdout);
+export const debug: LoggerLevel = new LoggerLevel(4, "debug", process.stdout);
+export const silly: LoggerLevel = new LoggerLevel(5, "silly", process.stdout);
 
 const _levels = new Map([
   ["silent", silent],
