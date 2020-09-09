4e0b4dfb7108f9b7406b619d367c33206e43fe7f
Kamontat Chantrachirathumrong <developer@kamontat.net>
2020-09-09 13:41:38 +0700

    perf(utils): deprecated the old way to update logger option

--- packages/logger/src/services/WithLogger.ts
+++ packages/logger/src/services/WithLogger.ts
@@ -1,19 +1,30 @@
 import { Logger } from "../models/logger/Logger";
-import { LoggerBuilder } from "../builder/LoggerBuilder";
+import { LoggerBuilder, UpdateOptionFn } from "../builder/LoggerBuilder";
 
 export abstract class WithLogger<T extends string = ""> {
   protected logger: Logger<T>;
 
-  constructor(builder: LoggerBuilder<T> = LoggerBuilder.initial()) {
+  constructor(private builder: LoggerBuilder<T> = LoggerBuilder.initial()) {
     this.logger = builder.get();
   }
 
   /**
+   * @deprecated
    * update current logger object
    *
    * @param fn perform anything you needs to create new logger object, logger in parameters has no side effect
    */
   protected updateLogger(fn: (newObject: Logger<T>) => Logger<T>): void {
     this.logger = fn(this.logger.copy());
+  }
+
+  /**
+   * update logger options and settings
+   *
+   * @param fn update logger option via builder
+   */
+  protected updateLoggerOption<R extends string>(fn: UpdateOptionFn<T, R>): void {
+    this.builder = this.builder.updateOption(fn);
+    this.logger = this.builder.get();
   }
 }
