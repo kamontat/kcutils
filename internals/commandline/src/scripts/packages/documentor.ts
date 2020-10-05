3b439cefe980a7615e1a232a973253358a3bf0d0
Kamontat Chantrachirathumrong <developer@kamontat.net>
2020-10-05 15:07:00 +0700

    feat(core): support publish document to gh pages after package been published

--- internals/commandline/src/scripts/packages/documentor.ts
+++ internals/commandline/src/scripts/packages/documentor.ts
@@ -2,38 +2,64 @@ import { AsyncRunner, Commandline, Option } from "../..";
 
 const option = new Option({ dirname: process.cwd(), input: process.argv.slice(2), transform: Option.transform });
 const transformer = new AsyncRunner(option, async ({ helper, data }) => {
-  const typedoc = helper.path.nodeCommand("typedoc");
+  if (data.arguments.publish) {
+    const publishTo = data.arguments.publish;
+    helper.log.debug("Publish", `starting publish to ${publishTo}`);
 
-  const project = helper.on("parent");
+    const name = helper.on("parent").projectName(undefined, false);
 
-  const tsconfig = project.pathEnsureSync("tsconfig.json");
-  const sourcecode = project.pathEnsureSync("src");
-  const readme = project.pathEnsureSync("README.md") ?? "none";
-  const result = project.path("docs");
+    const ghpages = helper.path.nodeCommand("gh-pages");
 
-  const args: string[] = [];
+    if (publishTo === "github") {
+      if (ghpages === undefined) return ["echo", "[skip] gh-pages command missing"];
+      else
+        return [
+          ghpages,
+          "--add",
+          "--message",
+          "chore(release): publish new documentation website [skip ci]",
+          "--dist",
+          "docs",
+          "--dest",
+          name,
+        ];
+    } else {
+      return ["echo", `unsupport publish to location: ${publishTo}`];
+    }
+  } else {
+    const typedoc = helper.path.nodeCommand("typedoc");
 
-  if (typedoc === undefined) return ["echo", `[skip] typedoc command not exist`];
-  else if (tsconfig === undefined) return ["echo", `[skip] tsconfig.json file is missing from ${project.pwd}`];
-  else if (sourcecode === undefined) return ["echo", `[skip] ${project.path("src")} is missing`];
-  args.push(
-    typedoc,
-    "--toc",
-    "--mode",
-    "modules",
-    "--out",
-    result,
-    "--tsconfig",
-    tsconfig,
-    "--readme",
-    readme,
-    sourcecode
-  );
+    const project = helper.on("parent");
 
-  // pass other arguments to commandline
-  args.push(...(data.arguments._ ?? []));
+    const tsconfig = project.pathEnsureSync("tsconfig.json");
+    const sourcecode = project.pathEnsureSync("src");
+    const readme = project.pathEnsureSync("README.md") ?? "none";
+    const result = project.path("docs");
 
-  return args;
+    const args: string[] = [];
+
+    if (typedoc === undefined) return ["echo", `[skip] typedoc command not exist`];
+    else if (tsconfig === undefined) return ["echo", `[skip] tsconfig.json file is missing from ${project.pwd}`];
+    else if (sourcecode === undefined) return ["echo", `[skip] ${project.path("src")} is missing`];
+    args.push(
+      typedoc,
+      "--toc",
+      "--mode",
+      "modules",
+      "--out",
+      result,
+      "--tsconfig",
+      tsconfig,
+      "--readme",
+      readme,
+      sourcecode
+    );
+
+    // pass other arguments to commandline
+    args.push(...(data.arguments._ ?? []));
+
+    return args;
+  }
 });
 
 const cli = new Commandline(transformer);
