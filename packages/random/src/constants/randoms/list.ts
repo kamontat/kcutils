6290cc3c82f97bfdcea7a847c011cd03c22ebb07
Kamontat Chantrachirathumrong <developer@kamontat.net>
2020-10-16 17:59:21 +0700

    fix(const): remove duplicate "0"

--- packages/random/src/constants/randoms/list.ts
+++ packages/random/src/constants/randoms/list.ts
@@ -56,4 +56,4 @@ export const englishUpperAlphabetCharacter = [
   "Z",
 ];
 
-export const numberCharacter = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "0"];
+export const numberCharacter = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
