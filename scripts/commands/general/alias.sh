185949385f08f70dc0b409b5cf6c836cdbf3b024
Kamontat Chantrachirathumrong <kamontat.chantrachirathumrong@agoda.com>
2021-11-21 02:38:01 +0700

    chore: add `general alias` for output all alias setup

---
+++ scripts/commands/general/alias.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+# shellcheck disable=SC1000
+
+# generate by create-script-file v4.0.1
+# link (https://github.com/Template-generator/create-script-file/tree/v4.0.1)
+
+# set -x #DEBUG - Display commands and their arguments as they are executed.
+# set -v #VERBOSE - Display shell input lines as they are read.
+# set -n #EVALUATE - Check syntax of the script but don't execute.
+
+echo
+echo "Alias information"
+for i in "${COMMAND_ALIAS[@]}"; do
+  key="${i%%=*}"
+  value="${i##*=}"
+  printf '  - %-5s => %s\n' "$key" "$value"
+done
+echo
