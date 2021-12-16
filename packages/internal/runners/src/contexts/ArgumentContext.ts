fe506821d86a45162ba5a018ab90302288e805af
Kamontat Chantrachirathumrong <developer@kamontat.net>
2021-12-16 10:12:33 +0000

    chore(docs): add document to argument context

--- packages/internal/runners/src/contexts/ArgumentContext.ts
+++ packages/internal/runners/src/contexts/ArgumentContext.ts
@@ -1,6 +1,9 @@
 import parser from "minimist";
 
-// value from minimist argument object
+/**
+ * @public
+ * default argument that minimist always returns
+ */
 export type DefaultArgument = {
   /**
    * If opts['--'] is true, populated with everything after the --
@@ -13,7 +16,17 @@ export type DefaultArgument = {
   _: string[];
 };
 
+/**
+ * @public
+ * command argument context
+ */
 export class ArgumentContext {
+  /**
+   * parse command argument (string[]) to json object
+   * @param data command argument
+   * @param opts option when this parse output
+   * @returns minimist object with default argument value
+   */
   parse<T>(data: string[], opts?: parser.Opts): T & DefaultArgument {
     return parser(data, opts) as unknown as T & DefaultArgument;
   }
