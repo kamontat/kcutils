1533bb1babb2a604d4f0a0ce753bc4ccc5d2a80a
Kamontat Chantrachirathumrong <developer@kamontat.net>
2021-12-26 14:11:10 +0700

    chore: reduce number of log

--- .github/actions/deployment/setup_git.sh
+++ .github/actions/deployment/setup_git.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
-echo '##### Config (Before change) #####'
-git config --local --list
+# echo '##### Config (Before change) #####'
+# git config --local --list
 
 if test -n "$GIT_USERNAME"; then
   git config --local user.name "$GIT_USERNAME"
@@ -22,5 +22,5 @@ if test -n "$GITHUB_TOKEN"; then
   git remote set-url origin "https://git:${GITHUB_TOKEN}@github.com/${GITHUB_REPOSITORY}.git"
 fi
 
-echo '##### Config (After change) #####'
-git config --local --list
+# echo '##### Config (After change) #####'
+# git config --local --list
