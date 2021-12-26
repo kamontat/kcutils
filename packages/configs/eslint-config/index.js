2d66ffec7b44988cce928039c797222515827b94
Kamontat Chantrachirathumrong <developer@kamontat.net>
2021-12-26 15:32:42 +0700

    fix(rule): update quote to allow backtick as well

--- packages/configs/eslint-config/index.js
+++ packages/configs/eslint-config/index.js
@@ -13,7 +13,7 @@ module.exports = {
   ],
   rules: {
     semi: ["error", "always"],
-    quotes: ["error", "double"],
+    quotes: ["error", "double", { allowTemplateLiterals: true }],
     "no-tabs": [
       "error",
       {
