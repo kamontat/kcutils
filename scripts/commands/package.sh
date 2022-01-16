ceefdb7f60d4f114f1de2e9f49b8ed0c4a750547
Kamontat Chantrachirathumrong <developer@kamontat.net>
2022-01-16 12:14:01 +0700

    perf(script): change yarn package to use nx command instead of yarn

--- scripts/commands/package.sh
+++ scripts/commands/package.sh
@@ -8,12 +8,11 @@
 # set -v #VERBOSE - Display shell input lines as they are read.
 # set -n #EVALUATE - Check syntax of the script but don't execute.
 
-log_warn "Module" "You using default command, which is yarn execution"
-
 module_name="$1"
-shift
+command_name="$2"
+shift 2
 
 log_info "Module" "Running on '$module_name'"
-on_module_directory "$module_name" &&
-  run_yarn "$@" &&
+on_root_directory &&
+  run_nx "run" "$module_name:$command_name" "$@" &&
   go_back
