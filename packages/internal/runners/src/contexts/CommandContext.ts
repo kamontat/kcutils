7a4d7fe9d1d763ff7a6aaa8b168a483f490f7c0f
Kamontat Chantrachirathumrong <developer@kamontat.net>
2022-01-16 19:58:13 +0700

    perf(apis): add command.tsc() and command.rollup() to cli context

--- packages/internal/runners/src/contexts/CommandContext.ts
+++ packages/internal/runners/src/contexts/CommandContext.ts
@@ -5,11 +5,29 @@
 export class CommandContext {
   /**
    * build node command with input argument
-   *
    * @param args node arguments
-   * @returns return node command
+   * @returns node command in cli
    */
   node(...args: string[]): string[] {
     return ["node", ...args];
+  }
+
+  /**
+   * build tsc command with input argument
+   * @param configPath tsconfig.json
+   * @param args tsc arguments
+   * @returns tsc command in cli
+   */
+  tsc(configPath: string, ...args: string[]): string[] {
+    return ["tsc", "--project", configPath, ...args];
+  }
+
+  /**
+   * build rollup command with input argument
+   * @param args rollup arguments
+   * @returns rollup command in cli
+   */
+  rollup(...args: string[]): string[] {
+    return ["rollup", "--config", ...args];
   }
 }
