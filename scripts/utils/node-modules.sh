8af05b0a502623127e3268335f225bf26fb4eaf7
Kamontat Chantrachirathumrong <developer@kamontat.net>
2022-01-16 11:20:54 +0700

    chore: new implement `run_node_module_bin` to use yarn apis internally

---
+++ scripts/utils/node-modules.sh
@@ -0,0 +1,27 @@
+#!/usr/bin/env bash
+# shellcheck disable=SC1000
+
+# generate by create-script-file v4.0.1
+# link (https://github.com/Template-generator/create-script-file/tree/v4.0.1)
+
+# set -x #DEBUG - Display commands and their arguments as they are executed.
+# set -v #VERBOSE - Display shell input lines as they are read.
+# set -n #EVALUATE - Check syntax of the script but don't execute.
+
+#/ -----------------------------------
+#/ Create by:    Kamontat Chantrachirathumrong <developer@kamontat.net>
+#/ Since:        10/12/2020
+#/ -----------------------------------
+
+export run_node_module_bin
+run_node_module_bin() {
+  local command_name command_args command_path
+  command_name="$1"
+  shift
+  command_args=("$@")
+  command_path="$(yarn bin "$command_name")"
+  if test -n "$command_path"; then
+    run_cmd "$command_path" "${command_args[@]}"
+  fi
+  return 1
+}
