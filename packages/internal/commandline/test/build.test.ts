b1fbb26331c837decc03d81668c894f3c4e80b8e
Kamontat Chantrachirathumrong <developer@kamontat.net>
2022-01-17 09:57:56 +0700

    fix(test): test is expect correct value

--- packages/internal/commandline/test/build.test.ts
+++ packages/internal/commandline/test/build.test.ts
@@ -9,7 +9,7 @@ describe("Build Controller", () => {
   });
   describe("Build Action", () => {
     test("constants", () => {
-      expect(action.name).toBe("option");
+      expect(action.name).toBe("action");
       expect(action.previous).toBeTruthy();
     });
   });
