9ca8f340030b552aa80c4444c41bd4d8936f1e98
Kamontat Chantrachirathumrong <developer@kamontat.net>
2022-01-21 23:27:58 +0700

    perf(model): migrate helper to @types/generic use internally

--- packages/typings/generic/index.d.ts
+++ packages/typings/generic/index.d.ts
@@ -1,3 +1,8 @@
 declare module "generic" {
-  export type Nullable<T> = T | undefined | null;
+  export type WithNull<T> = T | null;
+  export type WithUndefined<T> = T | undefined;
+  export type Null = undefined | null;
+
+  export type Optional<T> = T | undefined | null;
+  export type Nullable<T> = Optional<T>; // alias
 }
