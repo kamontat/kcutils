aa42f69420b6cb944e23a9e8cb56c1bdef5860cb
Kamontat Chantrachirathumrong <developer@kamontat.net>
2022-01-24 10:29:11 +0700

    perf(config): enabled @kcutils/random on browser mode

--- packages/public/random/rollup.config.js
+++ packages/public/random/rollup.config.js
@@ -2,4 +2,6 @@ const config = require("@kcconfig/rollup-config");
 const pkg = require("./package.json");
 
 module.exports = config.initial(
-  config.buildCommonJSModuleJS(pkg)  );
+  config.buildCommonJSModuleJS(pkg),
+  config.buildBrowser(pkg)
+);
