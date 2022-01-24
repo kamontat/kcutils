1d26ff8da48802ca1505e2092c0d51e49e7e1187
Kamontat Chantrachirathumrong <developer@kamontat.net>
2022-01-24 13:14:09 +0700

    feat(config): add overrided config option on output level and root level

--- packages/configs/rollup-config/index.js
+++ packages/configs/rollup-config/index.js
@@ -5,10 +5,10 @@ const { buildUnscopedName, buildFormat, buildPlugins } = require("./src/utils");
 /**
  * @argument {{
  *    pkg: any
- *    format: "umd" | "cjs" | "es"
- *    extraFormat?: "umd" | "cjs" | "es"
+ *    formats: ("umd" | "cjs" | "es")[]
  *    map?: boolean
  *    compact?: boolean
+ *    overrided?: import("rollup").OutputOptions
  * }} opt
  * @return {import("rollup").RollupOptions}
  */
@@ -16,37 +16,37 @@ function build(opt) {
   const name = buildUnscopedName(opt.pkg.name);
   const sourcemap = opt.map ? opt.map : true;
   const compact = opt.compact ? opt.compact : true;
-  const format = buildFormat(opt.pkg, opt.format);
-
   const input = opt.pkg.typedocMain;
-  if (!input) {
-    throw new Error(`Input cannot be undefined (type=${format.id})`);
+  if (opt.formats.length < 1) {
+    throw new Error(`Format cannot be undefined`);
+  } else if (!input) {
+    throw new Error(`Input cannot be undefined`);
   }
 
   /**
    * @type {import("rollup").OutputOptions[]}
    */
   const output = [];
-  output.push({
-    name,
-    file: format.outfile,
-    format: format.id,
-    sourcemap,
-    compact,
-  });
 
-  if (opt.extraFormat) {
-    const extraFormat = buildFormat(opt.pkg, opt.extraFormat);
+  /** @type {{[key: string]: boolean}} */
+  const checkDuplicate = {};
+  for (const format of opt.formats) {
+    if (!checkDuplicate[format]) {
+      checkDuplicate[format] = true;
+    }
+
+    const format = buildFormat(opt.pkg, opt.format);
     output.push({
       name,
-      file: extraFormat.outfile,
-      format: extraFormat.id,
+      file: format.outfile,
+      format: format.id,
       sourcemap,
       compact,
+      ...opt.overrided,
     });
   }
 
-  const plugins = buildPlugins(opt.format);
+  const plugins = buildPlugins(opt.formats[0]);
   return {
     input,
     output,
@@ -56,45 +56,51 @@ function build(opt) {
 
 /**
  * @param {any} pkg package.json content
+ * @param {import("rollup").OutputOptions[]} overrided custom rollup config
  */
-function buildBrowser(pkg) {
-  return build({ pkg, format: "umd", map: true, compact: true });
+function buildBrowser(pkg, overrided) {
+  return build({ pkg, formats: ["umd"], map: true, compact: true, overrided });
 }
 
 /**
  * @param {any} pkg package.json content
+ * @param {import("rollup").OutputOptions[]} overrided custom rollup config
  */
-function buildCommonJS(pkg) {
+function buildCommonJS(pkg, overrided) {
   return build({
     pkg,
-    format: "cjs",
+    formats: ["cjs"],
     map: true,
     compact: true,
+    overrided,
   });
 }
 
 /**
  * @param {any} pkg package.json content
+ * @param {import("rollup").OutputOptions[]} overrided custom rollup config
  */
-function buildModuleJS(pkg) {
+function buildModuleJS(pkg, overrided) {
   return build({
     pkg,
-    format: "es",
+    formats: ["es"],
     map: true,
     compact: true,
+    overrided,
   });
 }
 
 /**
  * @param {any} pkg package.json content
+ * @param {import("rollup").OutputOptions[]} overrided custom rollup config
  */
-function buildCommonJSModuleJS(pkg) {
+function buildCommonJSModuleJS(pkg, overrided) {
   return build({
     pkg,
-    format: "cjs",
-    extraFormat: "es",
+    formats: ["cjs", "es"],
     map: true,
     compact: true,
+    overrided,
   });
 }
 
@@ -107,10 +113,25 @@ function initial(...options) {
   return rollup.defineConfig(options);
 }
 
+/**
+ *
+ * @param {import("rollup").RollupOptions} overrided
+ * @param {import("rollup").RollupOptions[]} options
+ * @returns {import("rollup").RollupOptions[]}
+ */
+function dinitial(overrided, ...options) {
+  const results = options.map((opt) => {
+    return Object.assign(opt, overrided);
+  });
+
+  return initial(results);
+}
+
 module.exports = {
   buildBrowser,
   buildCommonJS,
   buildModuleJS,
   buildCommonJSModuleJS,
   initial,
+  dinitial,
 };
