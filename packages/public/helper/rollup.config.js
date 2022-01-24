b398daea14e9601135c91d113eb08ab36ea8868a
Kamontat Chantrachirathumrong <developer@kamontat.net>
2022-01-24 10:15:07 +0700

    feat(models): order deps, browser key depend on settings, add includes directory to package, add generator key in package.json

--- packages/public/helper/rollup.config.js
+++ packages/public/helper/rollup.config.js
@@ -1,5 +1,4 @@
 const config = require("@kcconfig/rollup-config");
 const pkg = require("./package.json");
 
-module.exports = config.initial(
-  config.buildCommonJSModuleJS(pkg)  );
+module.exports = config.initial(config.buildCommonJSModuleJS(pkg));
