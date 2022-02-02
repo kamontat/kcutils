c397092fa4dd9d1b98288c2dd42db570b0ceb27d
Kamontat Chantrachirathumrong <developer@kamontat.net>
2022-02-03 00:43:35 +0700

    perf(apis): toString() return better result when input is custom Object

--- packages/public/helper/src/string/toString.ts
+++ packages/public/helper/src/string/toString.ts
@@ -1,7 +1,16 @@
 import isNumber from "../number/isNumber";
 import isBoolean from "../boolean/isBoolean";
 import isObject from "../object/isObject";
+import stringify from "../object/stringify";
 import isString from "./isString";
+
+interface ObjWithToString {
+  toString: () => string;
+}
+const hasToString = (input: unknown): input is ObjWithToString => {
+  if (input === undefined || input === null) return false;
+  return !!(input as ObjWithToString).toString;
+};
 
 /**
  * Cast input to string. This supporting by listed below
@@ -10,16 +19,20 @@ import isString from "./isString";
  * 2. boolean | string => return as string
  * 3. number           => return as fix digit not more than 6
  * 4. array            => return as array syntax without space
- * 5. object           => return by JSON.stringify()
+ * 5. object           => return by object.stringify()
  *
  * @param input input data on any type
  */
 const toString = <T = unknown>(input: T): string | undefined => {
-  if (isString(input)) return input;
-  else if (isBoolean(input)) return input ? "true" : "false";
+  if (Array.isArray(input)) return `[${input.join(",")}]`;
   else if (isNumber(input)) return input.toString(10);
-  else if (Array.isArray(input)) return `[${input.join(",")}]`;
-  else if (isObject(input)) return JSON.stringify(input);
+  else if (isString(input)) return input;
+  else if (isBoolean(input)) return input ? "true" : "false";
+  // Must be JSON object, not custom object
+  else if (isObject(input) && (input as any).constructor === Object)
+    return stringify(input);
+  // Custom object should falling here
+  else if (hasToString(input)) return (input as ObjWithToString).toString();
   else return undefined;
 };
 
