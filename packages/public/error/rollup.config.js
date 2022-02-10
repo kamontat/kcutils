50c2e5a4d6cf959e1881ce5c9121f453cd9a7949
Kamontat Chantrachirathumrong <developer@kamontat.net>
2022-02-10 22:29:29 +0700

    fix(config): this is not optimize for browser usage yet

--- packages/public/error/rollup.config.js
+++ packages/public/error/rollup.config.js
@@ -1,7 +1,4 @@
 const config = require("@kcconfig/rollup-config");
 const pkg = require("./package.json");
 
-module.exports = config.initial(
-  config.buildCommonJSModuleJS(pkg),
-  config.buildBrowser(pkg)
-);
+module.exports = config.initial(config.buildCommonJSModuleJS(pkg));
