f750e0df93e1b2c32abebfc8a2cc98afbb83c8b5
Kamontat Chantrachirathumrong <developer@kamontat.net>
2022-02-10 22:03:29 +0700

    perf(apis): remove object collection of seeds and randoms, instead export them individually
    BREAKING CHANGE: All Seeds.XXX will changes to XXX and Randoms.YYY will changes to YYY

--- packages/public/random/index.ts
+++ packages/public/random/index.ts
@@ -12,26 +12,11 @@ export {
   englishLowerAlphabetCharacter,
 } from "./src/constants/randoms/list";
 
-import { Fixed } from "./src/constants/seeds/fixed";
-import { Xfnv1a } from "./src/constants/seeds/xfnv1a";
-import { Xmur3 } from "./src/constants/seeds/xmur3";
-import { Timestamp } from "./src/constants/seeds/timestamp";
+export { Fixed } from "./src/constants/seeds/fixed";
+export { Xfnv1a } from "./src/constants/seeds/xfnv1a";
+export { Xmur3 } from "./src/constants/seeds/xmur3";
+export { Timestamp } from "./src/constants/seeds/timestamp";
 
-const Seeds = {
-  Fixed, // eslint-disable-line @typescript-eslint/naming-convention
-  Xfnv1a, // eslint-disable-line @typescript-eslint/naming-convention
-  Xmur3, // eslint-disable-line @typescript-eslint/naming-convention
-  Timestamp, // eslint-disable-line @typescript-eslint/naming-convention
-};
-
-import { Alea } from "./src/constants/randoms/Alea";
-import { Xoshiro128PP } from "./src/constants/randoms/Xoshiro128PP";
-import { Xoshiro128SS } from "./src/constants/randoms/Xoshiro128SS";
-
-const Randoms = {
-  Alea, // eslint-disable-line @typescript-eslint/naming-convention
-  Xoshiro128PP, // eslint-disable-line @typescript-eslint/naming-convention
-  Xoshiro128SS, // eslint-disable-line @typescript-eslint/naming-convention
-};
-
-export { Seeds, Randoms };
+export { Alea } from "./src/constants/randoms/Alea";
+export { Xoshiro128PP } from "./src/constants/randoms/Xoshiro128PP";
+export { Xoshiro128SS } from "./src/constants/randoms/Xoshiro128SS";
