482a2a1f829722bcd9e8124a67077c49122c8882
Kamontat Chantrachirathumrong <developer@kamontat.net>
2022-02-23 23:13:31 +0700

    perf(deps): update pkg of generator output

--- packages/private/generator/src/models/pkg.ts
+++ packages/private/generator/src/models/pkg.ts
@@ -25,14 +25,14 @@ const internalPackageVersion = (category: string, version: string) =>
 const getDevDeps = (category: string, compiler: SupportedCompiler) => {
   const base: Record<string, string> = {};
 
-  base["@kcconfig/eslint-config"] = internalPackageVersion(category, "0.1.4");
-  base["@kcconfig/jest-config"] = internalPackageVersion(category, "0.1.4");
-  base["@kcconfig/stryker-config"] = internalPackageVersion(category, "0.1.4");
+  base["@kcconfig/eslint-config"] = internalPackageVersion(category, "0.1.5");
+  base["@kcconfig/jest-config"] = internalPackageVersion(category, "0.1.5");
+  base["@kcconfig/stryker-config"] = internalPackageVersion(category, "0.1.5");
   base["@kcconfig/ts-config"] = internalPackageVersion(category, "0.1.4");
   if (compiler === "rollup") {
-    base["@kcconfig/rollup-config"] = internalPackageVersion(category, "0.1.4");
+    base["@kcconfig/rollup-config"] = internalPackageVersion(category, "0.2.0");
   }
-  base["@kcinternal/commandline"] = internalPackageVersion(category, "0.23.1");
+  base["@kcinternal/commandline"] = internalPackageVersion(category, "0.23.2");
 
   base["@stryker-mutator/core"] = "5.6.1";
   base["@stryker-mutator/jest-runner"] = "5.6.1";
