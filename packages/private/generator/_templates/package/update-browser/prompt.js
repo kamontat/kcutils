236dd651d063938f442af2a8a8b7ca4b2a37b849
Kamontat Chantrachirathumrong <developer@kamontat.net>
2022-03-07 15:23:51 +0700

    feat(core): add new generator for update browser status

---
+++ packages/private/generator/_templates/package/update-browser/prompt.js
@@ -0,0 +1,25 @@
+// see types of prompts:
+// https://github.com/enquirer/enquirer/tree/master/examples
+
+const { repo } = require("../../../lib/src");
+
+module.exports = [
+  {
+    type: "autocomplete",
+    name: "package",
+    message: "What is your's package name?",
+    choices: repo.listPackages(),
+    limit: 5,
+  },
+  {
+    type: "confirm",
+    name: "browser",
+    message: (context) => {
+      const answers = context.answers;
+      const package = repo.getPackage(answers.package);
+      const mode = package.pjson.browser ? "nodejs" : "browser";
+      return `Change to '${mode}' mode?`;
+    },
+    initial: true,
+  },
+];
