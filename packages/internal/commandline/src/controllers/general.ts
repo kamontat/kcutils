b7d3bac3b953e05649875943ced6815c2e1dcb9b
Kamontat Chantrachirathumrong <developer@kamontat.net>
2022-03-07 13:18:36 +0700

    fix(core): remove version from package code because project layout on production is different

--- packages/internal/commandline/src/controllers/general.ts
+++ packages/internal/commandline/src/controllers/general.ts
@@ -3,7 +3,6 @@ import {
   ActionBuilder,
   Commandline,
   Help,
-  _version,
 } from "@kcinternal/runners";
 
 export const help = Help.initial("Help for kc-general").newParagraph(
@@ -15,11 +14,9 @@ export const option = OptionBuilder.empty().build();
 export const action = ActionBuilder.initial(option, async (_, context) => {
   try {
     const pkg = require("../../../package.json");
-    context.log.print(`Runner`, _version);
     context.log.print(`Commandline`, pkg.version);
   } catch (e) {
-    context.log.print(`Runner`, _version);
-    context.log.print(`Commandline`, "dev");
+    context.log.print(`Commandline`, "__dev__");
   }
 
   return [];
--- packages/internal/runners/index.ts
+++ packages/internal/runners/index.ts
@@ -29,5 +29,3 @@ export type {
   PackageRepository,
 } from "package_json";
 export type { Nullable } from "generic";
-
-export { version as _version } from "./package.json";
