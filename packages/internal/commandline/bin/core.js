27b5d9a4748f53641aedc2a9c0b7705145e7728c
Kamontat Chantrachirathumrong <developer@kamontat.net>
2022-05-08 00:38:04 +0700

    perf(script): improve error if command is not exist

--- packages/internal/commandline/bin/core.js
+++ packages/internal/commandline/bin/core.js
@@ -3,5 +3,12 @@
 const cmd = process.argv[2];
 if (cmd) {
   const args = process.argv.slice(3);
-  require("../lib/index.js")[cmd](args);
+  const index = require("../lib/index.js");
+
+  if (index[cmd]) {
+    index[cmd](args);
+  } else {
+    const keys = Object.keys(index).join(",");
+    throw new Error("expected '" + keys + "' but receive '" + cmd + "'");
+  }
 }
