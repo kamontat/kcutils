7c0945f21401ba3504c8926ef2a9006028d55d86
Kamontat Chantrachirathumrong <developer@kamontat.net>
2022-08-12 10:02:11 +0700

    fix(script): yarn install will fail if we not bootstrap them first

--- scripts/commands/packages/bootstrap.sh
+++ scripts/commands/packages/bootstrap.sh
@@ -10,6 +10,7 @@
 
 on_root_directory
 
+run_xlerna "bootstrap"                                              # bootstrap it together
 ! is_ci && run_xlerna_exec "--" "yarn" "install" "--prefer-offline" # run `yarn install` on every package in local
-run_xlerna "bootstrap" &&                                           # bootstrap it together
-  go_back
+
+go_back
